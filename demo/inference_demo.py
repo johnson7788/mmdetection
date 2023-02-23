400f79df9b4e9f5ef486ebda2929f54afbcc49d7
johnson <johnson@macbook.com>
2023-02-23 13:53:02 +0800

    2023-02-23_13:53

--- demo/inference_demo.py
+++ demo/inference_demo.py
@@ -11,7 +11,8 @@ import mmcv
 config_file = '../configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 # download the checkpoint from model zoo and put it in `checkpoints/`
 # url: https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth
-checkpoint_file = '../checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+# checkpoint_file = '../checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+checkpoint_file = 'cosmetic_exps/latest.pth'
 assert os.path.exists(checkpoint_file), f"checkpoint_file文件不存在，请检查: {checkpoint_file}"
 
 
@@ -23,7 +24,7 @@ model = init_detector(config_file, checkpoint_file, device='cuda:0')
 
 
 # test a single image
-img = 'demo.jpg'
+img = 'cosmetic/train/images/809848abdd9102af586c531cfcc8a69a.jpg'
 result = inference_detector(model, img)
 
 
@@ -31,5 +32,5 @@ result = inference_detector(model, img)
 
 
 # show the results
-show_result_pyplot(model, img, result, out_file="demo_res.png")
+show_result_pyplot(model, img, result, score_thr=0.6, out_file="demo_res.png")
 
